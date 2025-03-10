4dd2474d104cfaad7e17323a9400da98c2f62d73
jinseok469 <jinseok469@naver.com>
2025-03-10 11:01:48 +0900

    first

---
+++ staticfinal/src/main/java/com/staticfinal/StaticfinalApplication.java
@@ -0,0 +1,13 @@
+package com.staticfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticfinalApplication.class, args);
+	}
+
+}
---
+++ staticfinal/src/test/java/com/staticfinal/StaticfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staticfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
